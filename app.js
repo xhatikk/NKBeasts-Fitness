748c601321a4645b55aa5ed0d7de26f1f9ad95fd
xhatikk <xhatikk@hotmail.com>
2026-07-25 09:52:02 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const cookieBanner = document.getElementById('cookie');
-if (cookieBanner && localStorage.getItem('cookie-ok') === '1') {
-  cookieBanner.classList.add('hidden');
-}
-document.querySelector('[data-cookie-ok]')?.addEventListener('click', () => {
-  localStorage.setItem('cookie-ok', '1');
-  cookieBanner?.classList.add('hidden');
-});
